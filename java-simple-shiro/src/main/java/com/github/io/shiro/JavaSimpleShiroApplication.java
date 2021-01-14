57c00edc3b3654bf30c3df594869025fb6d37b2c
yangyiyin <yangyiyin@chainup.com>
2021-01-14 19:56:39 +0800

    modify

---
+++ java-simple-shiro/src/main/java/com/github/io/shiro/JavaSimpleShiroApplication.java
@@ -0,0 +1,13 @@
+package com.github.io.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSimpleShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSimpleShiroApplication.class, args);
+    }
+
+}
