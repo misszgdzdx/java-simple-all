a5d4118fe5af0cd06043dff4721e3e2fce6a89f6
yangyiyin <yangyiyin@chainup.com>
2021-01-14 09:43:29 +0800

    init

---
+++ src/main/java/com/github/io/parent/JavaSimpleAllApplication.java
@@ -0,0 +1,13 @@
+package com.github.io.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSimpleAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSimpleAllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/io/parent/JavaSimpleAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.io.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSimpleAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
